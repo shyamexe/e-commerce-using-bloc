bbadffcb7a52c1e7443e847d2fd008fd6eb1aaa0
shyamjith <shyamjith38@hotmail.com>
2021-11-13 23:43:14 +0530

    red color added

--- lib/core/constants/strings.dart
+++ lib/core/constants/strings.dart
@@ -9,5 +9,8 @@ class Strings {
   static final homeScreenCenterText = 'Welcome to Flutterly!';
 }
 class AppColors{
-  static Color greenColor = const Color(0xff53B175);
+  static Color greenColor = const Color(0xFF53B175);
+  static Color redColor   = const Color(0xFFF3603F);
+  static Color darkBlueColor = const Color(0xFF07094F);
+  static Color greyColor = const Color(0xFFE2E2E2);
 }
