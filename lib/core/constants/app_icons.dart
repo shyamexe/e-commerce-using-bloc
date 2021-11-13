12dd841a38400e8cde6bb795900af617514edb15
shyamjith <shyamjith38@hotmail.com>
2021-11-13 11:40:47 +0530

    icons test files added

---
+++ lib/core/constants/app_icons.dart
@@ -0,0 +1,8 @@
+class Strings {
+  // App
+  static final cartIcon = 'asset/icons/cart.png';
+
+  // HomeScreen
+  static final homeScreenTitle = 'Home Screen';
+  static final homeScreenCenterText = 'Welcome to Flutterly!';
+}
