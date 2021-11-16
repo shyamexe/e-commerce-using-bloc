24b9bedb97d55a3825dc5e5059cfac233b13c67c
jezeel <jesil0097@gmail.com>
2021-11-16 16:34:31 +0530

    just 16/11

--- lib/presentation/screens/home_screen/home_screen.dart
+++ lib/presentation/screens/home_screen/home_screen.dart
@@ -2,11 +2,9 @@ import 'package:e_commerce/core/constants/app_icons.dart';
 import 'package:e_commerce/presentation/screens/home_screen/widgets/home_appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:e_commerce/core/constants/strings.dart';
-import 'package:sizer/sizer.dart';
 
 class HomeScreen extends StatelessWidget {
-  HomeScreen({Key? key, required this.title}) : super(key: key);
+  const HomeScreen({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -14,6 +12,52 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: homeAppbar(context),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(15), topRight: Radius.circular(15)),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 1,
+                offset: Offset(0, 0.75), // changes position of shadow
+              ),
+            ],
+          ),
+          height: 55,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.add),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: SvgPicture.asset(
+                  AppIcons.shopIcon,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.home),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.settings),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: SvgPicture.asset(
+                  AppIcons.userIcon,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
@@ -29,22 +73,26 @@ class HomeScreen extends StatelessWidget {
                   boxShadow: const [
                     BoxShadow(
                       color: Colors.grey,
-                      offset: Offset(0.0, 1.0), //(x,y)
+                      offset: Offset(0, 0.75), //(x,y)
                       blurRadius: 4.0,
                     ),
                   ],
                 ),
                 child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 3),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 3),
                   child: Row(
                     children: [
-                      Icon(Icons.search_rounded),
-                      SizedBox(width: 15,),
+                      const Icon(Icons.search_rounded),
+                      const SizedBox(
+                        width: 15,
+                      ),
                       const Expanded(
                         child: TextField(
                           decoration: InputDecoration(
                               border: InputBorder.none,
-                              hintText: "What are you looking for?.."),),
+                              hintText: "What are you looking for?.."),
+                        ),
                       ),
                       IconButton(
                         onPressed: () {},
@@ -57,58 +105,85 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ),
               ),
-              SizedBox(height: 20,),
+              const SizedBox(
+                height: 20,
+              ),
               SizedBox(
                 height: 50,
                 child: Row(
                   children: [
-                    const Text("Welcome", style: TextStyle(
-                      fontWeight: FontWeight.bold, fontSize: 35, color: Colors.green
-                    ),),
-                    SvgPicture.asset(AppIcons.byeIcon,color: Colors.redAccent,),
+                    const Text(
+                      "Welcome",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 35,
+                          color: Colors.green),
+                    ),
+                    SvgPicture.asset(
+                      AppIcons.byeIcon,
+                      color: Colors.redAccent,
+                    ),
                   ],
                 ),
               ),
-              Align(
-                alignment: Alignment.topLeft,
-                  child: Text("Need a helping hand today?..", style: TextStyle(color: Colors.grey),)),
-              SizedBox(height: 25,),
-              Container(
-                height: 140,
-                width: 500,
-                decoration: BoxDecoration(
-                  color: Colors.indigo[900],
-                  borderRadius: BorderRadius.circular(15),)
-              ),
-              SizedBox(height: 25,),
-              Align(
+              const Align(
                   alignment: Alignment.topLeft,
-                  child: Text("Catogory", style: TextStyle(color: Colors.green,
-                      fontSize: 20, fontWeight: FontWeight.bold),)),
-              SizedBox(height: 15,),
+                  child: Text(
+                    "Need a helping hand today?..",
+                    style: TextStyle(color: Colors.grey),
+                  )),
+              const SizedBox(
+                height: 25,
+              ),
+              Container(
+                  height: 140,
+                  width: 500,
+                  decoration: BoxDecoration(
+                    color: Colors.indigo[900],
+                    borderRadius: BorderRadius.circular(15),
+                  )),
+              const SizedBox(
+                height: 25,
+              ),
+              const Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    "Catogory",
+                    style: TextStyle(
+                        color: Colors.green,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  )),
+              const SizedBox(
+                height: 15,
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   Expanded(
                     child: Container(
-                      height: 100,
+                        height: 100,
                         decoration: BoxDecoration(
                           color: Colors.green,
-                          borderRadius: BorderRadius.circular(10),)
-                    ),
+                          borderRadius: BorderRadius.circular(10),
+                        )),
                   ),
-                  SizedBox(width: 15,),
+                  const SizedBox(
+                    width: 15,
+                  ),
                   Expanded(
                     child: Container(
                         height: 100,
                         decoration: BoxDecoration(
                           color: Colors.green[200],
-                          borderRadius: BorderRadius.circular(10),)
-                    ),
+                          borderRadius: BorderRadius.circular(10),
+                        )),
                   ),
                 ],
               ),
-              SizedBox(height: 15,),
+              const SizedBox(
+                height: 15,
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
@@ -117,40 +192,55 @@ class HomeScreen extends StatelessWidget {
                         height: 70,
                         decoration: BoxDecoration(
                           color: Colors.green[200],
-                          borderRadius: BorderRadius.circular(10),)
-                    ),
+                          borderRadius: BorderRadius.circular(10),
+                        )),
                   ),
-                  SizedBox(width: 15,),
+                  const SizedBox(
+                    width: 15,
+                  ),
                   Expanded(
                     child: Container(
                         height: 70,
                         decoration: BoxDecoration(
                           color: Colors.green[200],
-                          borderRadius: BorderRadius.circular(10),)
-                    ),
+                          borderRadius: BorderRadius.circular(10),
+                        )),
                   ),
                 ],
               ),
-              SizedBox(height: 25,),
-              Align(
+              const SizedBox(
+                height: 25,
+              ),
+              const Align(
                   alignment: Alignment.topLeft,
-                  child: Text("Offers & New", style: TextStyle(color: Colors.green,
-                      fontSize: 20, fontWeight: FontWeight.bold),)),
-              SizedBox(height: 15,),
+                  child: Text(
+                    "Offers & New",
+                    style: TextStyle(
+                        color: Colors.green,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  )),
+              const SizedBox(
+                height: 15,
+              ),
               Row(
                 children: [
                   InkWell(
-                    onTap: (){},
+                    onTap: () {},
                     child: Container(
-                      padding: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10),
                       height: 40,
-                        decoration: BoxDecoration(
-                          color: Colors.green,
-                            border: Border.all(color: Colors.green.shade200),
-                          borderRadius: BorderRadius.circular(13),),
-                      child: Text("Trending", style: TextStyle(
-                        color: Colors.white, fontWeight: FontWeight.bold
-                      ),),
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        border: Border.all(color: Colors.green.shade200),
+                        borderRadius: BorderRadius.circular(13),
+                      ),
+                      child: const Text(
+                        "Trending",
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
                     ),
                   ),
                 ],
