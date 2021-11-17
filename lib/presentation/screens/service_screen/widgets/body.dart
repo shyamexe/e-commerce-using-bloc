aa92e2817f12852f2430699ea9e5b842d9646b4e
shyamjith <shyamjith38@hotmail.com>
2021-11-17 19:29:46 +0530

    list added

--- lib/presentation/screens/service_screen/widgets/body.dart
+++ lib/presentation/screens/service_screen/widgets/body.dart
@@ -6,18 +6,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
-
 class Body extends StatelessWidget {
-  const Body({ Key? key }) : super(key: key);
+  const Body({Key? key}) : super(key: key);
 
   @override
-     Widget build(BuildContext context) {
+  Widget build(BuildContext context) {
     return ListView(
       children: [
         BlocBuilder<ServiceselectionCubit, ServiceselectionState>(
           builder: (context, state) {
             return CupertinoSegmentedControl<int>(
-              
               groupValue: state.pageValue,
               borderColor: AppColors.greenColor,
               selectedColor: AppColors.greenColor,
@@ -31,34 +29,206 @@ class Body extends StatelessWidget {
               onValueChanged: (groupValue) {
                 print(groupValue);
 
-                
                 BlocProvider.of<ServiceselectionCubit>(context).update();
-                
               },
             );
           },
         ),
         Container(
-          margin: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
-          decoration: BoxDecoration(border: Border.all(color: AppColors.greyColor)),
+          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          decoration:
+              BoxDecoration(border: Border.all(color: AppColors.greyColor)),
           // width: 2.w,    //It will take a 20% of screen width
-          height:10.h,  
+          height: 10.h,
           child: Row(
             children: [
-              Image.asset(AppImage.lapTo1,width:100,height:100)
+              Image.asset(
+                AppImage.lapTo1,
+                width: 100,
+                height: 100,
+              )
             ],
-          ),  //It will take a 30% of screen height
-    )
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          decoration:
+              BoxDecoration(border: Border.all(color: AppColors.greyColor)),
+          
+          child:  Container(
+          height: 135,
+          margin: const EdgeInsets.all(10),
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(16.0),
+              topRight: Radius.circular(16.0),
+            ),
+          ),
+          child: Row(
+            children: [
+              Container(
+                width: 130,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(AppImage.desktop1),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+              Flexible(
+                child: Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Flexible(
+                            child: Text(
+                              'Thoshiba Tecra A50-D',
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w600, fontSize: 15),
+                            ),
+                          ),
+                          Material(
+                            color: Colors.transparent,
+                            child: InkWell(
+                              borderRadius: BorderRadius.circular(32.0),
+                              // splashColor: ,
+                              onTap: () {},
+                              child: Container(
+                                height: 50,
+                                width: 50,
+                                child: const Icon(
+                                  Icons.favorite,
+                                  color: Colors.red,
+                                  size: 22,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text('Price:'),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            '45055',
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.w600),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text('Sub Total:'),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            '450000',
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                                color:
+                                    // themeChange.darkTheme
+                                    //     ? Colors.brown.shade900
+                                    //:
+                                    Theme.of(context).accentColor),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            'Ships Free',
+                            style: TextStyle(
+                                color:
+                                    // themeChange.darkTheme
+                                    // ? Colors.brown.shade900
+                                    // :
+                                    Theme.of(context).accentColor),
+                          ),
+                          Spacer(),
+                          Material(
+                            color: Colors.transparent,
+                            child: InkWell(
+                              borderRadius: BorderRadius.circular(4.0),
+                              // splashColor: ,
+                              onTap: () {},
+                              child: Container(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(5.0),
+                                  child: Icon(
+                                    Icons.remove,
+                                    color: Colors.red,
+                                    size: 22,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Card(
+                            elevation: 12,
+                            child: Container(
+                              width: MediaQuery.of(context).size.width * 0.12,
+                              padding: const EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(colors: [
+                                  Colors.purple,
+                                  Colors.pink,
+                                ], stops: [
+                                  0.0,
+                                  0.7
+                                ]),
+                              ),
+                              child: Text(
+                                '154',
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ),
+                          Material(
+                            color: Colors.transparent,
+                            child: InkWell(
+                              borderRadius: BorderRadius.circular(4.0),
+                              // splashColor: ,
+                              onTap: () {},
+                              child: Container(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(5.0),
+                                  child: Icon(
+                                    Icons.add,
+                                    color: Colors.green,
+                                    size: 22,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
+        ),
 
+        //!ddd
+
+       
       ],
     );
   }
-  }
-
-
-
-
-
+}
 
 // class Body extends StatefulWidget {
 //   const Body({
@@ -70,7 +240,7 @@ class Body extends StatelessWidget {
 // }
 
 // class _BodyState extends State<Body> {
-  
+
 //   @override
 //   Widget build(BuildContext context) {
 //     return ListView(
@@ -78,7 +248,7 @@ class Body extends StatelessWidget {
 //         BlocBuilder<ServiceselectionCubit, ServiceselectionState>(
 //           builder: (context, state) {
 //             return CupertinoSegmentedControl<int>(
-              
+
 //               groupValue: state.pageValue,
 //               borderColor: AppColors.greenColor,
 //               selectedColor: AppColors.greenColor,
@@ -92,9 +262,8 @@ class Body extends StatelessWidget {
 //               onValueChanged: (groupValue) {
 //                 print(groupValue);
 
-                
 //                 BlocProvider.of<ServiceselectionCubit>(context).update();
-                
+
 //               },
 //             );
 //           },
